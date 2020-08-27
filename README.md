4722d3ecdbe9a5ae3de5d4197becde112e874084
eduedp <8978381+eduedp@users.noreply.github.com>
2020-08-27 15:50:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Skill Presets
+
+Save presets for glyphs and advanced skills.
+For example, you can save an entire preset for TANK and another for DPS.
+
+## Installation
+- Copy the .def file to `TeraToolbox\data\definitions`
+
+## Usage
+### `!presets` `!preset` `!pset`
+- Command prefix. You should add that before every command bellow
+### `list`
+- List saved presets
+### `save "name"` `add "name"`
+- Save current glyphs and advanced skills to a preset named "name"
+### `remove "name"` `rem "name"` `delete "name"` `del "name"`
+- Remove preset saved with that name
+### `"name"` `apply "name"` `set "name"`
+- Apply preset saved with that name
+Keep in mind that the Glyph Set current selected in Glyph Window will be overwritten
---
+++ index.js
@@ -0,0 +1,110 @@
+module.exports = function SkillPresets(mod) {
+
+    const command = mod.command || mod.require.command
+    const settings = mod.settings
+
+    if (!settings.presets || settings.presets === undefined) settings.presets = []
+
+    let activeAdvSkills,
+        activeGlyphs
+
+    mod.hook('S_RP_SKILL_POLISHING_LIST', 1, (e) => {
+        activeAdvSkills = e.optionEffects
+        console.log(activeAdvSkills)
+    })
+
+    mod.hook('S_CREST_INFO', 2, (e) => {
+        activeGlyphs = e.crests
+        console.log(activeGlyphs)
+    })
+
+    function savePreset(name) {
+        if (!name || name === undefined) {
+            command.message(`You must type a name for the preset.`)
+            return
+        }
+        if (!activeAdvSkills || !activeGlyphs) {
+            command.message(`Problem saving preset. Open glyph window and set glyphs and advanced skills first.`)
+            return
+        }
+
+        const glyphs = activeGlyphs.filter((e) => e.enable).map((e) => e.id)
+        const advSkills = activeAdvSkills.filter((e) => e.active).map((e) => ({ group:e.group, id: e.id }) )
+        const preset = settings.presets.find((e) => e.name == name)
+        if (preset) {
+            preset.glyphs = glyphs
+            preset.advSkills = advSkills
+        } else {
+            settings.presets.push({
+                name: name,
+                glyphs: glyphs,
+                advSkills: advSkills,
+            })
+        }
+
+        command.message(`Preset ${name} saved.`)
+    }
+
+    function applyPreset(name) {
+        const preset = settings.presets.find((e) => e.name == name)
+
+        if (!preset || preset === undefined) {
+            command.message(`Preset ${name} not found.` )
+            return
+        }
+
+        mod.send('C_CREST_APPLY_LIST', 2, {
+            glyphs: preset.glyphs
+        })
+
+        let delay = 250
+        preset.advSkills.forEach((e) => {
+            mod.setTimeout(() => {
+                mod.send('C_RQ_SKILL_POLISHING_CHANGE_OPTION', 1, {
+                    group: e.group,
+                    id: e.id,
+                    active: true,
+                })
+            }, delay += 250);
+        })
+
+        command.message(`Preset ${name} applied.` )
+    }
+
+    function removePreset(name) {
+        const preset = settings.presets.findIndex((e) => e.name == name)
+
+        if (!preset || preset === undefined) {
+            command.message(`Preset ${name} not found.`)
+            return
+        }
+
+        settings.presets.splice(preset, 1)
+    }
+
+    command.add(['presets', 'preset', 'pset'], (...values) => {
+        switch (values[0]) {
+            case 'add':
+            case 'save':
+                savePreset(values[1])
+                break
+            case 'remove':
+            case 'rem':
+            case 'delete':
+            case 'del':
+                removePreset(values[1])
+                break
+            case 'list':
+                command.message(`Preset list: ${settings.presets.map((e) => e.name).join(', ')}`)
+                console.log(settings.presets)
+                break
+            case 'apply':
+            case 'set':
+                applyPreset(values[1])
+                break
+            default:
+                applyPreset(values[0])
+                break
+        }
+    })
+}
